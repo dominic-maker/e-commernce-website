399ce08faa5045e8ddd876e8d8600085a5427d49
dominic-maker <dominicmuturi2018@gmail.com>
2022-05-23 12:38:00 +0300

    commit

--- DealPup/settings.py
+++ DealPup/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lm#=zlowu-_$0sb2yq2g*wrwk&#ne)p+k!fh6o@3hkd&il(ho-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
